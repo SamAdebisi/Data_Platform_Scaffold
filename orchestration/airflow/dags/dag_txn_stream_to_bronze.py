679d52171e4f146551e87009a36e0018479599bc
Azeez Adebisi <samadebisi12@gmail.com>
2025-09-04 05:09:45 +0300

    Update dag_txn_stream_to_bronze.py

--- orchestration/airflow/dags/dag_txn_stream_to_bronze.py
+++ orchestration/airflow/dags/dag_txn_stream_to_bronze.py
@@ -1,8 +1,16 @@
 from airflow import DAG
 from airflow.operators.python import PythonOperator
 from datetime import datetime
-import subprocess
+import subprocess, os
+
 def run_consumer():
-    subprocess.check_call(["python","/opt/airflow/dags/../..//streaming/consumers/txn_to_bronze/main.py"])
+    # Why: ensure env points to in-cluster services
+    env = {**os.environ}
+    subprocess.check_call(
+        ["python","/opt/airflow/streaming/consumers/txn_to_bronze/main.py"],
+        env=env
+    )
+
 with DAG("txn_stream_to_bronze", start_date=datetime(2024,1,1), schedule="@continuous", catchup=False) as dag:
     PythonOperator(task_id="consume_and_land", python_callable=run_consumer)
+    
